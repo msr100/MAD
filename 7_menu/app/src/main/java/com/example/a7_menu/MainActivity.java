af95d58fdb77d9997276efa0be80ff3a0173d93a
msr100 <officialartistirani+msr@gmail.com>
2026-06-21 15:29:26 +0000

    init

---
+++ 7_menu/app/src/main/java/com/example/a7_menu/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.a7_menu;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.Menu;
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
