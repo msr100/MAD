4b7f0f2f3eca3ba4dac51b09683269e3f60836e2
Big shot Basavaraj <officialartistirani+msr@gmail.com>
2026-06-22 22:40:23 +0530

    not required

--- 7_menu/app/src/main/java/com/example/a7_menu/MainActivity.java
+++ 7_menu/app/src/main/java/com/example/a7_menu/MainActivity.java
@@ -10,12 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 import android.view.Menu;
 public class MainActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-//    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
